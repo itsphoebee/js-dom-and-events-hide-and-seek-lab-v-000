2757e1fcbe93eef80e285a81d8e4cbafcf7896c8
Phoebe Lau <phoebelau7@gmail.com>
2018-03-29 22:23:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function getFirstSelector(selector){
 function nestedTarget(){
   return document.getElementById('nested').querySelector('div.target')
 }
+
+function deepestChild(){
+  return document.getElementById('grand-node').querySelectorAll('div')[3]
+}
